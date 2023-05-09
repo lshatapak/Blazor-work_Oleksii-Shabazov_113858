99cb65046163ad23e5f0c38cd12e1fa3cf05a414
lshatapak <sabazovaleksnj2@gmail.com>
2023-05-09 21:58:32 +0200

    Добавьте файлы проекта.

---
+++ Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Blazor_work_Oleksii_Shabazov_113858.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
